551a2b3949e15159670e942322f142c7db861192
Tony <tonytje9@hotmail.com>
2018-02-15 11:47:48 +0100

    Initial commit

---
+++ app/src/main/java/dehaagsehogeschool/cyberdemo/HomeActivity.java
@@ -0,0 +1,13 @@
+package dehaagsehogeschool.cyberdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
