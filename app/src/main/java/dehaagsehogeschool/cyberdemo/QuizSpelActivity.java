3509f786822eb66c98c398c31731674872310c94
Tony <tonytje9@hotmail.com>
2018-03-07 16:41:41 +0100

    - Quiz spel highscore wordt lokaal bijgehouden dmv sharedpreference - Het is niet mogelijk om terug te gaan naar de vorige vraag - De app activity lifecycle is bij te houden in de run log. Handig voor debuggen - String values toegevoegd

--- app/src/main/java/dehaagsehogeschool/cyberdemo/QuizSpelActivity.java
+++ app/src/main/java/dehaagsehogeschool/cyberdemo/QuizSpelActivity.java
@@ -1,7 +1,9 @@
 package dehaagsehogeschool.cyberdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,38 +19,67 @@ import java.util.TimerTask;
 
 public class QuizSpelActivity extends AppCompatActivity {
 
+    public final static String TAG = QuizSpelActivity.class.getSimpleName();
+
     private QuizLibary quizLibary = new QuizLibary();
 
     private TextView quizScoreView;
     private TextView quizQuestionView;
+    private TextView quizQuestionNumber;
     private Button quizButtonChoice1;
     private Button quizButtonChoice2;
     private Button quizButtonChoice3;
     private Button quizButtonChoice4;
+    private Button quizStopButton;
 
     private String quizQuestionAnswer;
-    private int quizQuestionNumber = 0;
+    private int questionNumber = 1;
+    private int questionAnswerNumber = 0;
     public int score = 0;
-    private int time = 180;
-
+    private int time = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_spel_activity);
 
+        Log.i(TAG, "I am created!");
+
         quizQuestionView = (TextView) findViewById(R.id.quiz_spel_question);
+        quizQuestionNumber = (TextView) findViewById(R.id.quiz_spel_question_number);
         quizButtonChoice1 = (Button) findViewById(R.id.quiz_spel_choice1);
         quizButtonChoice2 = (Button) findViewById(R.id.quiz_spel_choice2);
         quizButtonChoice3 = (Button) findViewById(R.id.quiz_spel_choice3);
         quizButtonChoice4 = (Button) findViewById(R.id.quiz_spel_choice4);
+        quizStopButton = (Button) findViewById(R.id.quiz_spel_stop_button);
 
         updateQuestion();
         startTimer();
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "I am paused!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "I am stopped!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "I am destroyed!");
+    }
+
     public void quitGameClick(View view) {
+        Intent showScore = new Intent(getApplicationContext(), QuizSpelResultActivity.class);
+        showScore.putExtra("SCORE", score);
+        startActivity(showScore);
         finish();
     }
 
@@ -58,12 +89,6 @@ public class QuizSpelActivity extends AppCompatActivity {
 
             updateQuestion();
             score++;
-            Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
-
         }
     }
 
@@ -73,11 +98,6 @@ public class QuizSpelActivity extends AppCompatActivity {
 
             updateQuestion();
             score++;
-            Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
 
         }
     }
@@ -88,11 +108,6 @@ public class QuizSpelActivity extends AppCompatActivity {
 
             updateQuestion();
             score++;
-            Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
 
         }
     }
@@ -103,28 +118,32 @@ public class QuizSpelActivity extends AppCompatActivity {
 
             updateQuestion();
             score++;
-            Toast.makeText(this, "good", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
 
         }
     }
 
     private void updateQuestion() {
 
-        quizQuestionView.setText(quizLibary.getQuestion(quizQuestionNumber));
-        quizButtonChoice1.setText(quizLibary.getChoice1(quizQuestionNumber));
-        quizButtonChoice2.setText(quizLibary.getChoice2(quizQuestionNumber));
-        quizButtonChoice3.setText(quizLibary.getChoice3(quizQuestionNumber));
-        quizButtonChoice4.setText(quizLibary.getChoice4(quizQuestionNumber));
+        if (questionAnswerNumber < quizLibary.quizQuestions.length) {
 
-        quizQuestionAnswer = quizLibary.getAnswer(quizQuestionNumber);
-        quizQuestionNumber++;
+            quizQuestionView.setText(quizLibary.getQuestion(questionAnswerNumber));
+            quizButtonChoice1.setText(quizLibary.getChoice1(questionAnswerNumber));
+            quizButtonChoice2.setText(quizLibary.getChoice2(questionAnswerNumber));
+            quizButtonChoice3.setText(quizLibary.getChoice3(questionAnswerNumber));
+            quizButtonChoice4.setText(quizLibary.getChoice4(questionAnswerNumber));
 
+            quizQuestionAnswer = quizLibary.getAnswer(questionAnswerNumber);
+            quizQuestionNumber.setText("Vraag" + questionNumber);
+            questionNumber++;
+            questionAnswerNumber++;
+
+        } else {
+
+            //If elde tijdelijk gedaan omdat anders de applicatie crasht op nullpointer.
+            finish();
+
+        }
     }
-
     public void startTimer() {
         Timer t = new Timer();
 
@@ -140,22 +159,24 @@ public class QuizSpelActivity extends AppCompatActivity {
                         timer.setText("Tijd over:" + time + "");
                         if (time > 0) {
                             time -= 1;
-                        }
-                        else {
-                            timer.setText("Tijd om!");
-                            //TODO
+                        } else {
+                        cancel();
+                        finish();
+                        showScore();
                         }
                     }
                 });
             }
         };
 
+        Log.i(TAG, "test");
         t.scheduleAtFixedRate(task, 0, 1000);
     }
 
     public void showScore(){
-
-
+        Intent showScore = new Intent(getApplicationContext(), QuizSpelResultActivity.class);
+        showScore.putExtra("SCORE", score);
+        startActivity(showScore);
     }
-
 }
+
